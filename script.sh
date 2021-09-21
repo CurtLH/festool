0e4e5cb87ed2b59337f9d6b6a26e62d6ef3ef3bb
Curtis Hampton <CurtLHampton@gmail.com>
2021-09-20 21:31:25 -0700

    Change filename from script.py

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/curtis/github/festool/secrets.env
 source /home/curtis/miniconda3/bin/activate festool-dev
-python /home/curtis/github/festool/script.py
+python /home/curtis/github/festool/festool.py
